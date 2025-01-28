57fc147eda4bec116d82baf12d28d97266e76470
asa <asatogoshi@gmail.com>
2025-01-28 15:50:04 +0000

    po

--- frontend/react-store/src/lib/api.ts
+++ frontend/react-store/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const apiPublic = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
